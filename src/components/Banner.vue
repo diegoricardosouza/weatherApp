ccddce8ee782286becaa251dd77d1dc0a267721e
Diego Ricardo <diegoricardoweb@gmail.com>
2024-10-28 21:42:00 -0300

    feat: add component Banner

---
+++ src/components/Banner.vue
@@ -0,0 +1,19 @@
+<template>
+  <div
+      v-if="preview"
+      class="text-white p-4 bg-weather-secondary w-full text-center"
+    >
+      <p>
+        You are currently previewing this city, click the "+" icon to start tracking this city.
+      </p>
+    </div>
+</template>
+
+<script setup>
+defineProps({
+  preview: {
+    type: Boolean,
+    required: true
+  }
+})
+</script>
