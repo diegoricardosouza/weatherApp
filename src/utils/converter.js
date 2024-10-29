664ee37bbc8da21c968adb712affa713ff8a5829
Diego Ricardo <diegoricardoweb@gmail.com>
2024-10-28 21:43:48 -0300

    feat: add utils converter

---
+++ src/utils/converter.js
@@ -0,0 +1,25 @@
+export function fahrenheitToCelsius(fahrenheit) {
+  const converter = (Math.round(fahrenheit) - 32) * 5 / 9;
+  return String(converter).slice(0, 2)
+}
+
+export function converterDateFull(date) {
+  return new Date(date).toLocaleDateString(
+            "pt-br",
+            {
+              weekday: "long",
+              day: "2-digit",
+              month: "long",
+            }
+          );
+}
+
+export function converterHours(hours) {
+  return new Date(hours).toLocaleTimeString(
+            "pt-BR",
+            {
+              timeStyle: "short",
+              hour12: false
+            }
+          );
+}
