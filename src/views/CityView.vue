f2e3d836abe036503559255abd8aca8a53226d0b
Diego Ricardo <diegoricardoweb@gmail.com>
2024-10-28 21:43:34 -0300

    feat: add component CityView

---
+++ src/views/CityView.vue
@@ -0,0 +1,14 @@
+<template>
+  <Suspense>
+    <AsyncCityView />
+    <template #fallback>
+      <p>Loading...</p>
+    </template>
+  </Suspense>
+</template>
+
+<script setup>
+import AsyncCityView from '@/components/AsyncCityView.vue';
+
+
+</script>
