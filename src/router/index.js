1db783ae0bfe5d28c1a953333d513ecb0675b6b7
Diego Ricardo <diegoricardoweb@gmail.com>
2024-11-13 21:24:44 -0300

    feat: complete app

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,36 @@
-import CityView from '@/views/CityView.vue'
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import CityView from '@/views/CityView.vue';
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from '../views/HomeView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: "/",
+      name: "home",
+      component: HomeView,
+      meta: {
+        title: "Home",
+      },
     },
     {
-      path: '/weather/:state/:city',
-      name: 'cityView',
-      component: CityView
-    }
-  ]
-})
+      path: "/weather/:state/:city",
+      name: "cityView",
+      component: CityView,
+      meta: {
+        title: "Weather",
+      },
+    },
+  ],
+});
+
+router.beforeEach((to, from, next) => {
+  document.title = `${
+    to.params.state
+      ? `${to.params.city}, ${to.params.state}`
+      : to.meta.title
+  } | Clima Local`;
+  next();
+});
 
 export default router
